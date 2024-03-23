bb7239390157d9054830aab76eafd11bae9078f3
Stepan Ponomarev <stepan.ponomarev@corp.mail.ru>
2024-03-23 17:56:09 +0300

    ch

--- test/edu/assembler/prarser/TokenPaterntsTest.java
+++ test/edu/assembler/prarser/TokenPaterntsTest.java
@@ -4,42 +4,32 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public final class TokenPaterntsTest {
-    private static final String COMMENT_LINE = "// some comment";
-
-    private static final String EMPTY_LINE = "   \s \t";
-
-    private static final String A_COMMAND_LINE = "@128";
-
-    private static final String INVALID_A_COMMAND = "@";
-
-    private static final String INVALID_SYMBOL = "1:testSymbol";
-
     @Test
     public void parseCommentLine() {
-        Assertions.assertTrue(TokenPaternts.COMMENT.matcher(COMMENT_LINE).matches());
+        Assertions.assertTrue(TokenPaternts.COMMENT.matcher("// some comment").matches());
     }
 
     @Test
     public void parseEmptyLine() {
-        Assertions.assertTrue(TokenPaternts.EMPTY.matcher(EMPTY_LINE).matches());
+        Assertions.assertTrue(TokenPaternts.EMPTY.matcher("   \s \t").matches());
     }
 
     @Test
     public void parseAInstruction() {
-        Assertions.assertTrue(TokenPaternts.A_INSTRUCTION.matcher(A_COMMAND_LINE).matches());
+        Assertions.assertTrue(TokenPaternts.A_INSTRUCTION.matcher("@128").matches());
     }
 
     @Test
     public void parseTooShortAInstruction() {
-        Assertions.assertFalse(TokenPaternts.A_INSTRUCTION.matcher(INVALID_A_COMMAND).matches());
+        Assertions.assertFalse(TokenPaternts.A_INSTRUCTION.matcher("@").matches());
     }
 
     @Test
     public void parseValidSymbol() {
-        final String symbol = "symbol";
+        final String symbol = "testSymbol";
         Assertions.assertTrue(TokenPaternts.SYMBOL.matcher(symbol).matches());
 
-        final String[] correctStringFirstSymbols = {":", "$", "_"};
+        final String[] correctStringFirstSymbols = {":", "$", "_", "."};
         for (String start : correctStringFirstSymbols) {
             Assertions.assertTrue(TokenPaternts.SYMBOL.matcher(start + symbol).matches());
         }
@@ -47,10 +37,5 @@ public final class TokenPaterntsTest {
         for (int i = 0; i <= 9; i++) {
             Assertions.assertFalse(TokenPaternts.SYMBOL.matcher(i + symbol).matches());
         }
-    }
-
-    @Test
-    public void parseInvalidSymbol() {
-        Assertions.assertFalse(TokenPaternts.SYMBOL.matcher(INVALID_SYMBOL).matches());
     }
 }
