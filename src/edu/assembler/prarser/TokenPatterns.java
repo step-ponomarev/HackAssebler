7dc6e6a356035c3d8085fdb11012b59db3e731f7
Stepan Ponomarev <stepan.ponomarev@corp.mail.ru>
2024-11-14 19:47:25 +0300

    added empty line

--- src/edu/assembler/prarser/TokenPatterns.java
+++ src/edu/assembler/prarser/TokenPatterns.java
@@ -4,6 +4,7 @@ import java.util.regex.Pattern;
 
 public final class TokenPatterns {
     static Pattern SYMBOL = Pattern.compile("^[a-zA-Z_.$:][a-zA-Z_.$:0-9]*$");
+
     static Pattern DECIMAL = Pattern.compile("^[0-9]+$");
     
     static Pattern COMMENT = Pattern.compile("^\\/{2}.*$");
